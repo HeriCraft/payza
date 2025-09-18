21a78122bf543aabf569513f2903341b66f067ba
HeriCraft <heri@granix.io>
2025-09-18 20:42:42 +0300

    feat: emit UserCreatedEvent after the user registration

--- src/main/java/io/granix/user/UserService.java
+++ src/main/java/io/granix/user/UserService.java
@@ -1,5 +1,6 @@
 package io.granix.user;
 
+import io.granix.common.event.user.UserCreatedEvent;
 import io.granix.common.security.CryptoService;
 import io.granix.user.entity.Role;
 import io.granix.user.entity.RoleNames;
@@ -7,10 +8,9 @@ import io.granix.user.entity.UserEntity;
 import io.granix.user.entity.UserStatus;
 import io.quarkus.elytron.security.common.BcryptUtil;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -21,8 +21,6 @@ import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @ApplicationScoped
 public class UserService {
     @Inject
@@ -31,6 +29,26 @@ public class UserService {
     @Inject
     private CryptoService cryptoService;
 
+    @Inject
+    Event<UserCreatedEvent> userCreated;
+
+    public UserService(){ }
+
+    public UserService(UserRepository repository, CryptoService cryptoService){
+        this.repository = repository;
+        this.cryptoService = cryptoService;
+    }
+
+    public UserService(UserRepository repository, CryptoService cryptoService, Event<UserCreatedEvent> event){
+        this.repository = repository;
+        this.cryptoService = cryptoService;
+        this.userCreated = event;
+    }
+
+    /**
+     * After the user registration, we should emit UserCreatedEvent
+     * in order that others module, such as wallet and notification can do their jobs.
+     */
     @Transactional
     public UserEntity register(
             String msisdn,
@@ -47,8 +65,7 @@ public class UserService {
             throw new IllegalArgumentException("Phone number already in use");
 
         // Setup default Role
-        var dafaultRole = new Role();
-        dafaultRole.name = RoleNames.USER;
+        var dafaultRole = new Role(RoleNames.USER);
 
         // Setup User
         var user = new UserEntity();
@@ -61,6 +78,13 @@ public class UserService {
         user.roles.add(dafaultRole);
 
         repository.persist(user);
+
+        userCreated.fireAsync(new UserCreatedEvent(
+                user.id,
+                user.msisdnEncrypted,
+                user.emailEncrypted
+        ));
+
         return user;
     }
 
